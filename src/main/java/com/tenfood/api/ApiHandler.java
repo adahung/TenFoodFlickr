afa93d423e7aa0295b705853c7c340e7d14eccbb
adahung <adahung@yahoo-inc.com>
2013-08-04 20:42:36 +0800

    fix invalid json response

--- src/main/java/com/tenfood/api/ApiHandler.java
+++ src/main/java/com/tenfood/api/ApiHandler.java
@@ -31,9 +31,9 @@ public class ApiHandler extends HttpServlet {
         resp.getWriter().println("{ \"photos\": ");
         resp.getWriter().println(new ObjectMapper().writeValueAsString(result));
         resp.getWriter().println(",");
-        resp.getWriter().println("\"context\": {");
+        resp.getWriter().println("\"context\": ");
         resp.getWriter().println(new ObjectMapper().writeValueAsString(context.getMessages()));
-        resp.getWriter().println("}}");
+        resp.getWriter().println("}");
     }
 
     public static void main(String[] args) throws Exception{

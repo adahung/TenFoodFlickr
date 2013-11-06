337fe4455e63d495582a020b81ed064a90a5ec87
adahung <adahung@yahoo-inc.com>
2013-11-07 00:01:42 +0800

    Add unittest for search param api

--- src/test/java/com/tenfood/api/service/FlickrServiceTest.java
+++ src/test/java/com/tenfood/api/service/FlickrServiceTest.java
@@ -3,6 +3,9 @@ package com.tenfood.api.service;
 import com.tenfood.api.Context;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.testng.Assert.assertTrue;
 
 /**
@@ -16,11 +19,30 @@ public class FlickrServiceTest {
     @Test
     public void testGetSearchRequestURI() throws Exception {
         FlickrService service = new FlickrService();
-        String uriString = service.getSearchRequestURI("test", 1);
+        Context context = new Context();
+        context.setQueryMap(new HashMap<String, String[]>());
+        String uriString = service.getSearchRequestURI("test", 1, context);
 
         assertTrue(uriString.contains("method=flickr.photos.search"));
         assertTrue(uriString.contains("text=test"));
         assertTrue(uriString.contains("api_key="));
         assertTrue(uriString.contains("per_page=1"));
     }
+
+    @Test
+    public void testGetSearchRequestParamURI() throws Exception {
+        FlickrService service = new FlickrService();
+        Context context = new Context();
+        Map<String, String[]> queryMap = new HashMap<String, String[]>();
+        queryMap.put("sort", new String[] { "aaa" });
+        context.setQueryMap(queryMap);
+        String uriString = service.getSearchRequestURI("test", 1, context);
+
+        assertTrue(uriString.contains("method=flickr.photos.search"));
+        assertTrue(uriString.contains("text=test"));
+        assertTrue(uriString.contains("api_key="));
+        assertTrue(uriString.contains("per_page=1"));
+        assertTrue(uriString.contains("sort=aaa"));
+    }
+
 }
